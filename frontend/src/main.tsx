5c812d08a43e4fbc01111efc9f23cfe71de84af3
sametsavgaman <153109295+sametsavgaman@users.noreply.github.com>
2026-09-03 16:18:17 +0300

    Add category tree and MITRE vine catalog visuals

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,5 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./docs.css";
+import "./catalog-visuals.css";
 
 createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
